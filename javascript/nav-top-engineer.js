b0d4e4c4f83b512336261fc750945d97ed9386b4
Leo Mathot <113390921+leomathot@users.noreply.github.com>
2024-11-17 04:41:22 -0500

    Add pages for Engineering Section.

--- javascript/nav-top-engineer.js
+++ javascript/nav-top-engineer.js
@@ -4,10 +4,48 @@ document.querySelector("#nav-top").innerHTML = `
         <a href="index.html">
             <img src="images/Crossfire-logo-no-bg.png" alt="Crossfire logo"/>
         </a>
-        <div class="links d-flex align-items-center">
-            <a href="#" title="User" role="button">
-                <i class="fa-solid fa-user"></i>
+
+        <div class="links d-flex align-items-center" id="user">
+            
+            <a class="text-black d-flex align-items-start" id="user-info" href="#">
+                <i class="fa-solid fa-user text-crossfire-primary pb-2"></i>
+
+                <span class="small ms-3">
+                    <div>John Davis</div>
+                    <div class="fw-bold text-mid-blue">Engineering</div>
+                </span>
             </a>
-            <span class="text-mid-blue fw-bold mt-3 ms-2 card p-1">Engineer</span>
+            
+            <div id="user-menu-container">
+                <div class="hidden" id="user-menu">
+                    <ul class="small">
+                        <li class="p-2">
+                            <a class="text-black d-flex align-items-center" href="#">
+                                <i class="fa-solid fa-message text-crossfire-secondary pb-2 me-2" style="font-size: 1rem;"></i>
+                                Notifications
+                            </a>
+                        </li>
+                        <li class="p-2 mt-1">
+                            <a class="text-black d-flex align-items-center" href="#">
+                                <i class="fa-solid fa-address-card text-crossfire-secondary pb-2 me-2" style="font-size: 1rem;"></i>
+                                Profile
+                            </a>
+                        </li>
+                        <li class="p-2 mt-1">
+                            <a class="text-black d-flex align-items-center" href="#">
+                                <i class="fa-solid fa-gear text-crossfire-secondary pb-2 me-2" style="font-size: 1rem;"></i>
+                                Settings
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div?
+
         </div>
     </div>`
+
+document.querySelector("#user-info").addEventListener("click", () => {
+    const userMenu = document.querySelector("#user-menu")
+    userMenu.classList.toggle("hidden")
+})
+      
--- javascript/nav-top.js
+++ javascript/nav-top.js
@@ -1,12 +1,16 @@
 
 document.querySelector("#nav-top").innerHTML = `
     <div class="width-limit">
+
         <a href="index.html">
             <img src="images/Crossfire-logo-no-bg.png" alt="Crossfire logo"/>
         </a>
-        <div class="links d-flex align-items-center">
-            <a href="#" title="User" role="button">
-                <i class="fa-solid fa-user"></i>
+
+        <div class="links d-flex align-items-center py-2" id="user">
+            
+            <a class="text-black d-flex align-items-start" id="user-info" href="#">
+                <i class="fa-solid fa-user text-crossfire-primary pb-2"></i>
             </a>
         </div>
+
     </div>`
